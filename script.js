bf06d4945a1ec49bdef589fc7e85f287b331210a
andrepane <156331723+andrepane@users.noreply.github.com>
2026-08-26 11:09:17 +0200

    Add event listener to loadExamples button

---
+++ script.js
@@ -0,0 +1,9 @@
+const status = document.getElementById("status");
+
+document.getElementById("loadExamples")
+.addEventListener("click",()=>{
+
+status.textContent=
+"En la siguiente fase aquí aparecerán los PIAT cargados.";
+
+});
